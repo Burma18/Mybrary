10f2ae5d1761cbaa28a61cada2ff2ff6a61461ce
Burma Rysmatova <rysmatovaburma@gmail.com>
2023-04-16 20:46:26 +0600

    books create routes new

--- routes/books.js
+++ routes/books.js
@@ -20,10 +20,16 @@ router.get("/", async (req, res) => {
   if (req.query.title != null && req.query.title != "") {
     query = query.regex("title", new RegExp(req.query.title, "i"));
   }
+  if (req.query.publishedBefore != null && req.query.publishedBefore != "") {
+    query = query.lte("publishDate", req.query.publishedBefore);
+  }
+  if (req.query.publishedAfter != null && req.query.publishedAfter != "") {
+    query = query.lte("publishDate", req.query.publishedAfter);
+  }
   try {
     const books = await query.exec();
     res.render("books/index", { books: books, searchOptions: req.query });
-  } catch (error) {
+  } catch {
     res.redirect("/");
   }
 });
